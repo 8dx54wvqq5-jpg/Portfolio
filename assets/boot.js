066e2a0c86aa6cf0c89c3d65bc75462740529d55
Abhikant Nirbhavane <abhikant.n@gmail.com>
2026-07-10 13:39:36 -0400

    Extract shared footer and boot scripts

---
+++ assets/boot.js
@@ -0,0 +1,12 @@
+(function () {
+  window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
+
+  (function (c, l, a, r, i, t, y) {
+    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments); };
+    t = l.createElement(r);
+    t.async = 1;
+    t.src = '/api/clarity/www/tag/' + i;
+    y = l.getElementsByTagName(r)[0];
+    y.parentNode.insertBefore(t, y);
+  })(window, document, 'clarity', 'script', 'xjjno0k0vd');
+})();
